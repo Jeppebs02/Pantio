9d9cb0e141d7b570e37ae5d853e2890b25b30885
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2026-05-01 13:28:20 +0200

    mapper og saa videre

---
+++ backend/PantioRepository/EntityFramework/Repositories/ItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PantioRepository.EntityFramework.Repositories;
+
+public class ItemRepository
+{
+
+}
---
+++ backend/PantioRepository/Mapper/Deleteme2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PantioRepository.Mapper;
+
+internal class Deleteme2
+{
+    // 
+}
