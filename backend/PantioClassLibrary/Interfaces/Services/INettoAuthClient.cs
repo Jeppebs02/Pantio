19e26c6b2c0e7339c76418bae5745c46358a85d8
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2026-06-09 20:13:27 +0200

    refactor NettoAuthClient, moved some stuff

---
+++ backend/PantioClassLibrary/Interfaces/Services/INettoAuthClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PantioClassLibrary.DTO;
+
+namespace PantioClassLibrary.Interfaces.Services;
+
+public interface INettoAuthClient
+{
+    Task<NettoTokenSet> ExchangeCodeAsync(string authorizationCode, string codeVerifier, string? redirectUri, CancellationToken ct = default);
+    Task<NettoTokenSet> RefreshAsync(string refreshToken, CancellationToken ct = default);
+    Task<IReadOnlyCollection<NettoReceiptSummary>> GetReceiptSummariesAsync(string accessToken, string idToken, CancellationToken ct = default);
+    Task<NettoReceiptDetail> GetReceiptDetailAsync(string accessToken, string idToken, string receiptType, string receiptId, CancellationToken ct = default);
+}
